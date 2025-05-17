d200c4bd35f1ecbfb8ae3df8d21e2837a47c9d6c
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2025-05-17 19:29:09 +0900

    refactor: 이미지 프리로드

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,56 @@
+// app/_layout.tsx
 import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 import { useFonts } from "expo-font";
 import { Tabs } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { View } from "react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function Layout() {
+  const [assetsLoaded, setAssetsLoaded] = useState(false);
+
   const [fontsLoaded] = useFonts({
     Cafe24Dongdong: require("../../assets/fonts/Cafe24Dongdong.ttf"),
   });
 
+  useEffect(() => {
+    async function loadAssets() {
+      try {
+        await Asset.loadAsync([
+          require("@/assets/images/emotion-happy.png"),
+          require("@/assets/images/emotion-calm.png"),
+          require("@/assets/images/emotion-sad.png"),
+          require("@/assets/images/emotion-angry.png"),
+          require("@/assets/images/emotion-neutral.png"),
+          require("@/assets/images/day.png"),
+        ]);
+      } catch (e) {
+        console.warn("이미지 로딩 오류:", e);
+      } finally {
+        setAssetsLoaded(true);
+      }
+    }
+    loadAssets();
+  }, []);
+
   const onLayoutRootView = useCallback(async () => {
-    if (fontsLoaded) {
+    if (fontsLoaded && assetsLoaded) {
       await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [fontsLoaded, assetsLoaded]);
 
-  if (!fontsLoaded) return null;
+  if (!fontsLoaded || !assetsLoaded) return null;
 
   return (
     <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
       <Tabs
         screenOptions={{
           tabBarStyle: {
-            backgroundColor: "#F5E3B3", // 탭 바 색
-            borderTopWidth: 0, // 윗 경계선 제거
+            backgroundColor: "#F5E3B3",
+            borderTopWidth: 0,
             height: 90,
             paddingTop: 10,
           },
