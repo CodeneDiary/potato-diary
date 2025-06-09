c542f78e3ff6b7e436bd2803a69b5b96905c3aab
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2025-06-09 15:54:59 +0900

    feat: 감정분석 그래프 구현, 감정 감자 이모지 하나 더 추가

--- constants/emotionImages.ts
+++ constants/emotionImages.ts
@@ -1,4 +1,10 @@
-export type EmotionType = "happy" | "sad" | "calm" | "angry" | "neutral";
+export type EmotionType =
+  | "happy"
+  | "sad"
+  | "calm"
+  | "angry"
+  | "neutral"
+  | "anxious";
 
 export const emotionImages: Record<EmotionType, any> = {
   happy: require("@/assets/images/emotion-happy.png"),
@@ -6,4 +12,5 @@ export const emotionImages: Record<EmotionType, any> = {
   calm: require("@/assets/images/emotion-calm.png"),
   angry: require("@/assets/images/emotion-angry.png"),
   neutral: require("@/assets/images/emotion-neutral.png"),
+  anxious: require("@/assets/images/emotion-anxious.png"),
 };
